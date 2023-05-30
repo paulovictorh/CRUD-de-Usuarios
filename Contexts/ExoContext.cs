4e81dff8e42e68614e5ac59f1d083c9d948ac4ac
Victor <paulovictorh12@gmail.com>
2023-05-30 14:47:43 -0300

    CRUDdeUsuarios

--- Contexts/ExoContext.cs
+++ Contexts/ExoContext.cs
@@ -4,29 +4,30 @@ using System.Data.SqlClient;
 using Microsoft.Data.SqlClient;
 namespace Exo.WebApi.Contexts
 {
-public class ExoContext : DbContext
-{
-public ExoContext()
-{
-}
-public ExoContext(DbContextOptions<ExoContext> options) : base(options)
-{
-}
-protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-{
-if(!optionsBuilder.IsConfigured)
+    public class ExoContext : DbContext
     {
-    // Essa string de conexão depende da SUA máquina.
-    optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;" + "Database=ExoApi;Trusted_Connection=True;");
-    // Exemplo 1 de string de conexão:
-    // User
-    // ID=sa;Password=admin;Server=localhost;Database=ExoApi;-
-    // + Trusted_Connection=False;
-    // Exemplo 2 de string de conexão:
-    //
-    // Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;
+        public ExoContext()
+        {
+        }
+        public ExoContext(DbContextOptions<ExoContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Essa string de conexão depende da SUA máquina.
+                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;" + "Database=ExoApi;Trusted_Connection=True;");
+                // Exemplo 1 de string de conexão:
+                // User
+                // ID=sa;Password=admin;Server=localhost;Database=ExoApi;-
+                // + Trusted_Connection=False;
+                // Exemplo 2 de string de conexão:
+                //
+                // Server=localhost\\SQLEXPRESS;Database=ExoApi;Trusted_Connection=True;
+            }
+        }
+        public DbSet<Projeto> Projetos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
     }
-}
-public DbSet<Projeto> Projetos { get; set; }
-}
 }
